c2dc7217b87ce0d4a90575471966de2aba178d2e
mykeels <mykehell123@gmail.com>
2021-08-24 08:10:44 +0100

    chore: add main colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "m-blue": {
+          400: "#0055C1",
+          700: "#0055BA"
+        },
+        "m-gray": {
+          400: "#D9E3EF"
+        }
+      }
+    },
   },
   variants: {
     extend: {},
