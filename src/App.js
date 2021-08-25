5717a1edb88665f22fd9d49aeb03b6e75b69e59b
mykeels <mykehell123@gmail.com>
2021-08-25 11:18:37 +0100

    feat: add robot bg image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import { Nav } from './common/components/Nav';
+import { Nav } from "./common/components/Nav";
 
 function App() {
   return (
-    <div>
+    <div className="bg-m-blue-400 h-screen bg-robot-image">
       <Nav />
+      <div className="block text-white">
+        <div className="inline-block w-1/2 align-top">
+          <h1 className="text-5xl font-sans py-24 px-12 leading-snug font-semibold">
+            Give Floof access to your financial data
+          </h1>
+        </div>
+        <div className="inline-block w-1/2 align-top"></div>
+      </div>
     </div>
   );
 }
