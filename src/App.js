cc847f211546c20a3092ff232ed44ebca33cb774
mykeels <mykehell123@gmail.com>
2021-08-26 12:01:54 +0100

    feat: use api to search for banks

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import { PermissionWidget } from "./common/components/PermissionWidget";
 import logoSVG from "./assets/images/logo.svg";
 
 function App() {
+  const getBanks = (search) =>
+    fetch(`${process.env.REACT_APP_API_URL}/banks?search=${search}`)
+      .then((res) => res.json())
+      .then((body) => {
+        console.log(body);
+        return body.data;
+      });
+
   return (
     <div className="bg-m-blue-400 min-h-screen bg-robot-image">
       <Nav />
       <div className="block text-white">
         <div className="inline-block w-full lg:w-1/2 align-top">
-          <h1
-            className="text-3xl lg:text-5xl font-sans py-2 lg:py-24 px-12 leading-snug lg:font-semibold w-full lg:w-128"
-          >
+          <h1 className="text-3xl lg:text-5xl font-sans py-2 lg:py-24 px-12 leading-snug lg:font-semibold w-full lg:w-128">
             Give Floof access to your financial data
           </h1>
         </div>
         <div className="inline-block w-full lg:w-1/2 align-top text-gray-800 px-4 sm:px-8 md:px-24 lg:px-0 py-8 font-mono text-center">
-          <PermissionWidget />
+          <PermissionWidget getBanks={getBanks} />
           <div className="py-4">
             <span className="inline-block text-white bg-m-blue-500 px-2 py-1 rounded">
               Powered by <img className="inline h-4" src={logoSVG} alt="Logo" />
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./styles.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
